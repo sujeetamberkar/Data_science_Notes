d4f65bbd3219125627ae89ce3964717ec24d115e
Sujeet Sanjay Amberkar <sujeet.amberkar2002@outlook.com>
2023-06-07 13:08:04 +0530

    Create createDatabase.py

---
+++ Basic_Python/DataBase/SQL/createDatabase.py
@@ -0,0 +1,11 @@
+import mysql.connector
+# import mysql.connector
+#create user 'user'@'%' identified by 'password'
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="abc",
+  password="password"
+)
+print(mydb)
+mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE IF  NOT EXISTS  test2.table_1(id INT, name VARCHAR(20), email_id VARCHAR(50))")
